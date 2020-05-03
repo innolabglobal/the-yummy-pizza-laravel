1ec3756d8b0609b44388c503f0634ba347e44448
JY Liu <ljieyao0210@gmail.com>
2020-05-03 22:04:49 +0800

    add package infyomlabs/generator-builder

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder')
+    ->name('io_generator_builder');
+
+Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate')
+    ->name('io_field_template');
+
+Route::get('relation_field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@relationFieldTemplate')
+    ->name('io_relation_field_template');
+
+Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate')
+    ->name('io_generator_builder_generate');
+
+Route::post('generator_builder/rollback', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@rollback')
+    ->name('io_generator_builder_rollback');
+
+Route::post('generator_builder/generate-from-file', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generateFromFile')
+    ->name('io_generator_builder_generate_from_file');
