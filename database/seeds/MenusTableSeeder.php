19afbc44fac1d8c1ad3350b25869826cf4796d09
JY Liu <ljieyao0210@gmail.com>
2020-05-04 10:17:22 +0800

    complete MenusTableSeeder

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 
 const DUMMY_PIZZAS = [
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_AM.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_AM.jpg',
         'name'        => 'CHICKENSAURUS',
         'description' => 'Roasted Chicken, Chicken Pepperoni and Mushroom Slices on our awesome Smoky Blended BBQ Sauce (Surcharge Applies).',
         'priceOption' => [
@@ -20,7 +20,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_UH.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_UH.jpg',
         'name'        => 'ULTIMATE HAWAIIAN',
         'description' => 'Loads of delicious roasted chicken, shredded chicken juicy pineapples and fresh mushrooms on our brand new pizza.',
         'priceOption' => [
@@ -35,9 +35,9 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_MT.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_MT.jpg',
         'name'        => 'MEATASAURUS',
-        'description' > 'Generous portions of everyone\'s favorite beef pepperoni, ground beef and fresh mushrooms on our new blended smoky BBQ sauce (Surcharge Applies)',
+        'description' => 'Generous portions of everyone\'s favorite beef pepperoni, ground beef and fresh mushrooms on our new blended smoky BBQ sauce (Surcharge Applies)',
         'priceOption' => [
             [
                 'priceName'  => 'small',
@@ -50,7 +50,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_GF.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_GF.jpg',
         'name'        => 'CHICKEN CONFIDENTIAL',
         'description' => 'Onions, cherry tomatoes, pineapple, chicken potpourri, roasted chicken and shredded chicken on our delicious Top Secret Sauce',
         'priceOption' => [
@@ -65,7 +65,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_ST.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_ST.jpg',
         'name'        => 'SAMBAL SURF &amp; TURF',
         'description' => 'Succulent Prawns marinated in herbs &amp; spices, Shredded Chicken, Juicy Pineapples and Green Peppers on our new and authentic sambal sauce. (Surcharge Applies)',
         'priceOption' => [
@@ -80,7 +80,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_PU.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_PU.jpg',
         'name'        => 'PRAWN SENSATION',
         'description' => 'Succulent Prawns marinated in Italian herbs &amp; spices, imported Belgian spinach, juicy cherry tomatoes &amp; onions with our brand new pizza sauce! (Surcharge Applie',
         'priceOption' => [
@@ -95,7 +95,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_SU.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_SU.jpg',
         'name'        => 'TROPICAL SAMBAL PRAWN',
         'description' => 'Succulent Prawns marinated in herbs &amp; spices, Fresh Onions, juicy Pineapples, Red Chillies on our new and authentic sambal sauce.',
         'priceOption' => [
@@ -110,7 +110,7 @@ const DUMMY_PIZZAS = [
         ]
     ],
     [
-        'image' > 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_PP.jpg',
+        'image'       => 'https://cache.dominos.com/olo/6_19_3/assets/build/market/MY/_en/images/img/products/thumbnails/S_PP.jpg',
         'name'        => 'PRAWN PASSION',
         'description' => 'Succulent prawns, marinated in Italian herbs and spices, juicy cherry tomatoes &amp; onions on our new pesto passion sauce! (Surcharge Applies)',
         'priceOption' => [
@@ -136,11 +136,20 @@ class MenusTableSeeder extends Seeder
     public function run ()
     {
         foreach (DUMMY_PIZZAS as $menu) {
-            Menu::firstOrCreate([
+            $newMenu = Menu::firstOrCreate([
                 'name'        => $menu['name'],
                 'description' => $menu['description'],
-                'image'       => $menu['image']
+                'image'       => $menu['image'],
             ]);
+
+            foreach ($menu['priceOption'] as $option) {
+                $newMenu->priceOption()->create(
+                    [
+                        'name'  => $option['priceName'],
+                        'value' => $option['priceValue'],
+                    ]
+                );
+            }
         }
     }
 
